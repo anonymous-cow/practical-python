aedf8c49cd33d6dda4e14e2866a552cd3c49f9fd
anonymous-cow <ashley.sungji.oh@gmail.com>
2021-03-03 13:39:55 -0800

    1.7 exercises

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,26 @@
 # pcost.py
 #
 # Exercise 1.27
+
+
+import sys
+import csv
+def portfolio_cost(filename):
+    with open (filename) as f:
+        rows = csv.reader(f)
+        cost = 0
+        for row in rows:
+            try:
+                #row=line.split(',')
+                cost = cost+int(row[1]) *float(row[2])
+            except ValueError:
+                print("warning, invalid literal")
+            print(row)
+        return(cost)
+
+if len(sys.argv)==2:
+    filename= sys.argv[1]
+else:
+    filename='practical-python/Work/Data/portfolio.csv'
+cost = portfolio_cost(filename)
+print('Total cost:', cost)
